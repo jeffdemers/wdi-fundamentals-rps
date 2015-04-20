85e6eb0fc953d251e7e106831bea929e70f0ab5e
Jeff Demers <jeffdemers@Jeffs-MBP-2.home>
2015-04-20 10:10:35 -0700

    playtofive added tie and win msg

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -69,6 +69,7 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+    var tie = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
 
@@ -80,15 +81,24 @@ while (playerWins < 5 && computerWins < 5){
     
     if (winner === 'player'){
         playerWins += 1;    
-    } else {
+    } 
+    else if (winner === 'computer'){
         computerWins += 1;
     } 
+
 console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
     console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
 
 }
+if (playerWins === 5){
+    console.log ("You win!");
+}
+else if (computerWins === 5){
+    console.log ("Computer wins!");
+}
     return [playerWins, computerWins];
 }
+
 
 
 
